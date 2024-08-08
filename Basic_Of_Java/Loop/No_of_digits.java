254ca4dba3f43514e7bf646d5289c53030fe7ac6
DEEPAK <152033387+MDeepak23@users.noreply.github.com>
2024-08-08 11:51:56 +0530

    Update No_of_digits.java

--- Basic_Of_Java/Loop/No_of_digits.java
+++ Basic_Of_Java/Loop/No_of_digits.java
@@ -3,7 +3,7 @@ class No_of_digits{
 public static void main(String[] args){
 Scanner sc = new Scanner(System.in);
 int num = sc.nextInt();
-int rem,count=0;;
+int rem,count=0;
 while(num!=0){
 rem = num%10;
 System.out.println(rem+" ");
