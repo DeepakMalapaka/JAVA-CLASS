0cf39480438e551246f720d57043afcde7af1d83
Anuram Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-08-08 11:25:46 +0530

    Create Instance.java

---
+++ Basic_Of_Java/Variables/Instance.java
@@ -0,0 +1,11 @@
+class Instance
+{
+    int a=10;
+    String b="CSE";
+    public static void main(String[] args) 
+    {
+     Instance obj=new Instance();
+     System.out.println("Enter value of A : "+obj.a);
+     System.out.println(obj.b); 
+    }
+}
