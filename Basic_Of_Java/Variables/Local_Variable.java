b5c4c877623b54e36fa218fee1379c8cd1098f19
Anuram Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-08-08 11:26:40 +0530

    Create Local_Variable.java

---
+++ Basic_Of_Java/Variables/Local_Variable.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Local_Variable
+{
+    public static void main(String[] args) 
+    {
+      int a;
+      int b;
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter value of A : ");
+      a=sc.nextInt();
+      System.out.println("Enter value of B : ");
+      b=sc.nextInt();
+      System.out.println("The value of A is "+a);
+      System.out.println(b);
+    }
+
+}
