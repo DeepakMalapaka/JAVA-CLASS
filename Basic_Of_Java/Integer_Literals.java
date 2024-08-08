2a74a86da32fbf7d146e19652f8ee6d1733f8c72
Anuram Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-08-08 11:27:09 +0530

    Create Integer_Literals.java

---
+++ Basic_Of_Java/Integer_Literals.java
@@ -0,0 +1,15 @@
+class Integer_Literals
+{
+    public static void main(String[] args) 
+    {
+      int a=10;
+      int b=010;
+      int c=0x10;
+      int d=0B001;
+      System.out.println("Decimal : "+a);
+      System.out.println("Octal : "+b);
+      System.out.println("Hexadecimal : "+c);
+      System.out.println("Binary : "+d);
+
+    }
+}
