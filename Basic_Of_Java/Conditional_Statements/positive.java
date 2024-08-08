4b0c81ca1c8ec24c10153391228ddae4b7d60dc6
DeepakMalapaka <154827923+DeepakMalapaka@users.noreply.github.com>
2024-08-08 11:46:44 +0530

    Update positive.java

--- Basic_Of_Java/Conditional_Statements/positive.java
+++ Basic_Of_Java/Conditional_Statements/positive.java
@@ -2,7 +2,7 @@ import java.util.*;
 class positive {
     public static void main(String args[]) {
         Scanner sc =new Scanner(System.in);
-        System.out.println("Enter a number is positive or not:");
+        System.out.println("Enter a number to check either positive or not:");
         int n=sc.nextInt();
         if(n>0) {
             System.out.println("The number is +ve");
