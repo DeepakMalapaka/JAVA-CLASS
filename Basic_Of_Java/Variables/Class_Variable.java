8691619c6d0b7faacea54fadfaff6b407fc42390
Anuram Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-08-08 11:25:16 +0530

    Create Class_Variable.java

---
+++ Basic_Of_Java/Variables/Class_Variable.java
@@ -0,0 +1,11 @@
+class Class_Variable
+{
+   static  int a=60;
+   static boolean b;
+    public static void main(String[] args) 
+    {
+     System.out.println("Enter value of A : "+a);
+     System.out.println(b); 
+    }
+
+}
