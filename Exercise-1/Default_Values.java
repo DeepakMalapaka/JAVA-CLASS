92d89a7cba2f0da8b8b345c7ee2d309836ab7c79
Anuram Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-08-08 11:34:19 +0530

    Create Default_Values.java

---
+++ Exercise-1/Default_Values.java
@@ -0,0 +1,23 @@
+//import java.util.*;
+class Default_Values
+{
+    static boolean a;
+    static byte b;
+    static char c;
+    static short d;
+    static int e;
+    static float f;
+    static long g;
+    static double h;
+    public static void main(String[] args) 
+    {
+        System.out.println("Boolean"+a);
+        System.out.println("Byte "+b);
+        System.out.println("Char"+c);
+        System.out.println("Short "+d);
+        System.out.println("Int"+e);
+        System.out.println("float "+f);
+        System.out.println("Long"+g);
+        System.out.println("Double "+h);
+    }
+}
