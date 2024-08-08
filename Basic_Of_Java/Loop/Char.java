50e4e55ac30ed4ededc28456950176988784ebd4
kundetigowtham06 <152044248+kundetigowtham06@users.noreply.github.com>
2024-08-08 12:18:17 +0530

    Add files via upload

---
+++ Basic_Of_Java/Loop/Char.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Char{
+	public static void main(String args[]){
+	char i;
+	System.out.println("Alphabets from A to Z=");
+	for(i='A';i<='Z';i++){
+	System.out.print(i+" ");
+	}
+	}
+	}
+	
