6da88dc98b3dc74285bf36b15318fa8752e8e460
DeepakMalapaka <154827923+DeepakMalapaka@users.noreply.github.com>
2024-08-08 11:42:43 +0530

    Add files via upload

---
+++ Basic_Of_Java/Conditional_Statements/positive.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class positive {
+    public static void main(String args[]) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter a number is positive or not:");
+        int n=sc.nextInt();
+        if(n>0) {
+            System.out.println("The number is +ve");
+        } 
+    }
+}
